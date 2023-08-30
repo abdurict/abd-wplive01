812e0ef03dbc337b808d58a57d2d6530e6992848
abdurict <it.razzaq@gmail.com>
2023-08-30 06:45:58 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+//MIXITUP
+var mixer = mixitup('.work-grid');
+
+//NAVIGATION
+
+function openMenu(){
+    document.getElementById("menu-container").style.width = "100%";
+}
+function closeMenu(){
+    document.getElementById("menu-container").style.width = "0";
+}
+
